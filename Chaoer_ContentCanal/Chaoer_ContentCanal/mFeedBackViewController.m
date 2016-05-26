573b35a6be4d4ee19b1f589a68c36bfc7ff32599
Mac <574989766@qq.com>
2016-05-26 10:48:31 +0800

    modify feedback page

--- Chaoer_ContentCanal/Chaoer_ContentCanal/mFeedBackViewController.m
+++ Chaoer_ContentCanal/Chaoer_ContentCanal/mFeedBackViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "mFeedBackViewController.h"
-
+#import "mFeedBackCell.h"
 #import "mFeedManageViewController.h"
 #import "mFeedCompanyViewController.h"
 
@@ -16,7 +16,7 @@
 #import "feedbackViewController.h"
 #import "needCodeViewController.h"
 #import "verifyBankViewController.h"
-@interface mFeedBackViewController ()
+@interface mFeedBackViewController ()<UITableViewDelegate,UITableViewDataSource>
 
 @end
 
@@ -29,40 +29,29 @@
     self.hiddenlll = YES;
     self.hiddenTabBar = YES;
     self.Title = self.mPageName = @"投诉建议";
+    
+    [self initView];
+}
+
+- (void)initView{
+
+    NSArray *mTT = @[@"投诉居民",@"投诉物管",@"对公司的建议"];
+    
+    [self.tempArray addObjectsFromArray:mTT];
+    
+    
+    [self loadTableView:CGRectMake(0, 64, DEVICE_Width, DEVICE_Height-64) delegate:self dataSource:self];
+    self.tableView.backgroundColor = [UIColor colorWithRed:0.93 green:0.94 blue:0.96 alpha:1.00];
+//    self.tableView.separatorStyle = UITableViewCellSelectionStyleNone;
+    UINib   *nib = [UINib nibWithNibName:@"mFeedBackCell" bundle:nil];
+    [self.tableView registerNib:nib forCellReuseIdentifier:@"cell"];
+    
+    [self.tableView reloadData];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-#pragma mark----投诉个人
-- (IBAction)mPerson:(id)sender {
-    if (![mUserInfo backNowUser].mIsRegist) {
-        [self AlertViewShow:@"未实名认证！" alertViewMsg:@"通过认证即可使用更多功能？" alertViewCancelBtnTiele:@"取消" alertTag:10];
-        return;
-    }
-
-    
-    mFeedPersonViewController *mmm = [[mFeedPersonViewController alloc] initWithNibName:@"mFeedPersonViewController" bundle:nil];
-    [self pushViewController:mmm];
-    
-}
-#pragma mark----投诉物管
-- (IBAction)mCanal:(id)sender {
-    if (![mUserInfo backNowUser].mIsRegist) {
-        [self AlertViewShow:@"未实名认证！" alertViewMsg:@"通过认证即可使用更多功能？" alertViewCancelBtnTiele:@"取消" alertTag:10];
-        return;
-    }
-    mFeedManageViewController *mmm = [[mFeedManageViewController alloc] initWithNibName:@"mFeedManageViewController" bundle:nil];
-    [self pushViewController:mmm];
-    
-}
-#pragma mark----投诉公司
-- (IBAction)mCompany:(id)sender {
-    
-    UIStoryboard *secondStroyBoard=[UIStoryboard storyboardWithName:@"Main" bundle:nil];
-    feedbackViewController *f =[secondStroyBoard instantiateViewControllerWithIdentifier:@"xxx"];
-    [self.navigationController pushViewController:f animated:YES];
 }
 
 /*
@@ -104,5 +93,73 @@
     al.tag = tag;
     [al show];
 }
+
+
+
+
+#pragma mark -- tableviewDelegate
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView              // Default is 1 if not implemented
+{
+    return 1;
+}
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    
+    return self.tempArray.count;
+    
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 45;
+    
+}
+
+-(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSString *reuseCellId = @"cell";
+    
+    mFeedBackCell *cell = [tableView dequeueReusableCellWithIdentifier:reuseCellId];
+    
+    cell.mTTT.text = self.tempArray[indexPath.row];
+    
+    
+    return cell;
+    
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+
+    if (indexPath.row == 0) {
+        if (![mUserInfo backNowUser].mIsRegist) {
+            [self AlertViewShow:@"未实名认证！" alertViewMsg:@"通过认证即可使用更多功能？" alertViewCancelBtnTiele:@"取消" alertTag:10];
+            return;
+        }
+        
+        
+        mFeedPersonViewController *mmm = [[mFeedPersonViewController alloc] initWithNibName:@"mFeedPersonViewController" bundle:nil];
+        [self pushViewController:mmm];
+    }else if (indexPath.row == 1){
+        if (![mUserInfo backNowUser].mIsRegist) {
+            [self AlertViewShow:@"未实名认证！" alertViewMsg:@"通过认证即可使用更多功能？" alertViewCancelBtnTiele:@"取消" alertTag:10];
+            return;
+        }
+        mFeedManageViewController *mmm = [[mFeedManageViewController alloc] initWithNibName:@"mFeedManageViewController" bundle:nil];
+        [self pushViewController:mmm];
+        
+    }else if (indexPath.row == 2){
+        UIStoryboard *secondStroyBoard=[UIStoryboard storyboardWithName:@"Main" bundle:nil];
+        feedbackViewController *f =[secondStroyBoard instantiateViewControllerWithIdentifier:@"xxx"];
+        [self.navigationController pushViewController:f animated:YES];
+        
+    }
+    
+    
+    
+    
+}
+
 
 @end
