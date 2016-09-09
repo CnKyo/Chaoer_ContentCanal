6b3143e2e29acfac8e9e136964c052a98a13c538
瞿伦平 <574989766@qq.com>
2016-09-09 11:24:11 +0800

    修改sharesdk分享功能

--- Chaoer_ContentCanal/Chaoer_ContentCanal/ViewController.m
+++ Chaoer_ContentCanal/Chaoer_ContentCanal/ViewController.m
@@ -27,16 +27,9 @@
 #import <ShareSDK/ShareSDK.h>
 #import <ShareSDKConnector/ShareSDKConnector.h>
 
-//腾讯开放平台（对应QQ和QQ空间）SDK头文件
-#import <TencentOpenAPI/TencentOAuth.h>
-#import <TencentOpenAPI/QQApiInterface.h>
-
 //微信SDK头文件
 #import "WXApi.h"
 
-//新浪微博SDK头文件
-#import "WeiboSDK.h"
-//新浪微博SDK需要在项目Build Settings中的Other Linker Flags添加"-ObjC"
 
 #import "CRSA.h"
 #import "Base64.h"
--- Chaoer_ContentCanal/Chaoer_ContentCanal/barCodeViewController.m
+++ Chaoer_ContentCanal/Chaoer_ContentCanal/barCodeViewController.m
@@ -10,6 +10,11 @@
 #import "mBarCodeView.h"
 #import "barCodeCell.h"
 #import <ShareSDK/ShareSDK.h>
+#import <ShareSDKExtension/SSEShareHelper.h>
+#import <ShareSDKUI/ShareSDK+SSUI.h>
+#import <ShareSDKUI/SSUIShareActionSheetStyle.h>
+#import <ShareSDKUI/SSUIShareActionSheetCustomItem.h>
+#import <ShareSDK/ShareSDK+Base.h>
 
 @interface barCodeViewController ()<UITableViewDelegate,UITableViewDataSource,UIActionSheetDelegate>
 
@@ -24,7 +29,7 @@
     BOOL isYes;
     
     NSString *mBarCodeURL;
-
+    UIImage *mBarCodeImg;
 }
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -93,6 +98,8 @@
 
 - (void)mWechat:(UIButton *)sender{
     MLLog(@"微信");
+    
+
 }
 
 - (void)mTencent:(UIButton *)sender{
@@ -100,12 +107,129 @@
 }
 - (void)mWebo:(UIButton *)sender{
     MLLog(@"微博");
+    
+    NSMutableDictionary *params = [self shareParams];
+    [ShareSDK share:SSDKPlatformTypeSinaWeibo
+         parameters:params
+     onStateChanged:^(SSDKResponseState state, NSDictionary *userData, SSDKContentEntity *contentEntity, NSError *error) {
+         
+     }];
+}
+
+
+-(NSMutableDictionary *)shareParams
+{
+    if (mBarCodeImg == nil) {
+        NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:mBarCodeURL]];
+        mBarCodeImg = [UIImage imageWithData:data];
+    }
+    
+    
+    NSMutableDictionary *shareParams = [NSMutableDictionary dictionary];
+    //[shareParams SSDKEnableUseClientShare];
+    NSArray* imageArray = @[mBarCodeImg];
+    [shareParams SSDKSetupShareParamsByText:@"分享内容"
+                                     images:imageArray
+                                        url:[NSURL URLWithString:@"http://www.mob.com"]
+                                      title:@"分享标题"
+                                       type:SSDKContentTypeImage];
+    return shareParams;
+}
+-(void)shareMethod
+{
+    NSMutableDictionary *params = [self shareParams];
+    
+    [ShareSDK showShareActionSheet:self.view
+                             items:nil
+                       shareParams:params
+               onShareStateChanged:^(SSDKResponseState state, SSDKPlatformType platformType, NSDictionary *userData, SSDKContentEntity *contentEntity, NSError *error, BOOL end) {
+                   
+                   switch (state) {
+                           
+                       case SSDKResponseStateBegin:
+                       {
+                           //[theController showLoadingView:YES];
+                           break;
+                       }
+                       case SSDKResponseStateSuccess:
+                       {
+                           //Facebook Messenger、WhatsApp等平台捕获不到分享成功或失败的状态，最合适的方式就是对这些平台区别对待
+                           if (platformType == SSDKPlatformTypeFacebookMessenger)
+                           {
+                               break;
+                           }
+                           
+                           UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"分享成功"
+                                                                               message:nil
+                                                                              delegate:nil
+                                                                     cancelButtonTitle:@"确定"
+                                                                     otherButtonTitles:nil];
+                           [alertView show];
+                           break;
+                       }
+                       case SSDKResponseStateFail:
+                       {
+                           if (platformType == SSDKPlatformTypeSMS && [error code] == 201)
+                           {
+                               UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"分享失败"
+                                                                               message:@"失败原因可能是：1、短信应用没有设置帐号；2、设备不支持短信应用；3、短信应用在iOS 7以上才能发送带附件的短信。"
+                                                                              delegate:nil
+                                                                     cancelButtonTitle:@"OK"
+                                                                     otherButtonTitles:nil, nil];
+                               [alert show];
+                               break;
+                           }
+                           else if(platformType == SSDKPlatformTypeMail && [error code] == 201)
+                           {
+                               UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"分享失败"
+                                                                               message:@"失败原因可能是：1、邮件应用没有设置帐号；2、设备不支持邮件应用；"
+                                                                              delegate:nil
+                                                                     cancelButtonTitle:@"OK"
+                                                                     otherButtonTitles:nil, nil];
+                               [alert show];
+                               break;
+                           }
+                           else
+                           {
+                               UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"分享失败"
+                                                                               message:[NSString stringWithFormat:@"%@",error]
+                                                                              delegate:nil
+                                                                     cancelButtonTitle:@"OK"
+                                                                     otherButtonTitles:nil, nil];
+                               [alert show];
+                               break;
+                           }
+                           break;
+                       }
+                       case SSDKResponseStateCancel:
+                       {
+                           UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"分享已取消"
+                                                                               message:nil
+                                                                              delegate:nil
+                                                                     cancelButtonTitle:@"确定"
+                                                                     otherButtonTitles:nil];
+                           [alertView show];
+                           break;
+                       }
+                       default:
+                           break;
+                   }
+                   
+                   if (state != SSDKResponseStateBegin)
+                   {
+                       //[theController showLoadingView:NO];
+                       //[theController.tableView reloadData];
+                   }
+                   
+               }];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
 
 /*
 #pragma mark - Navigation
@@ -119,16 +243,18 @@
 
 - (void)rightBtnTouched:(id)sender{
     
-    UIButton *btn = sender;
-    btn.selected = !btn.selected;
-    btn.selected = !isYes;
-    if (btn.selected) {
-        [self shaowShareView];
-        isYes = YES;
-    }else{
-        [self hiddenSahreView];
-        isYes = NO;
-    }
+    [self shareMethod];
+    
+//    UIButton *btn = sender;
+//    btn.selected = !btn.selected;
+//    btn.selected = !isYes;
+//    if (btn.selected) {
+//        [self shaowShareView];
+//        isYes = YES;
+//    }else{
+//        [self hiddenSahreView];
+//        isYes = NO;
+//    }
     
 
 //    UIActionSheet *acc = [[UIActionSheet alloc]initWithTitle:@"是否将图片保存到相册？" delegate:self cancelButtonTitle:@"取消" destructiveButtonTitle:nil otherButtonTitles:@"保存到相册", nil];
@@ -194,7 +320,9 @@
     
     [cell.mHeader sd_setImageWithURL:[NSURL URLWithString:url] placeholderImage:[UIImage imageNamed:@"icon_headerdefault"]];
     
-    [cell.mBarCode sd_setImageWithURL:[NSURL URLWithString:mBarCodeURL] placeholderImage:[UIImage imageNamed:@"DefaultImg"]];
+    [cell.mBarCode sd_setImageWithURL:[NSURL URLWithString:mBarCodeURL] placeholderImage:[UIImage imageNamed:@"DefaultImg"] completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, NSURL *imageURL) {
+        mBarCodeImg = image;
+    }];
     
     cell.mNickName.text = [mUserInfo backNowUser].mNickName;
     cell.mIdentify.text = [mUserInfo backNowUser].mIdentity;
