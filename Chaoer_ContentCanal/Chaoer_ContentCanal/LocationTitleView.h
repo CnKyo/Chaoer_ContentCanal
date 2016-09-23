0418daa39b1682eb77eea5b83bf77f51008b66b9
Mac <574989766@qq.com>
2016-09-23 11:46:40 +0800

    准备加入饿了吗定位titleview

---
+++ Chaoer_ContentCanal/Chaoer_ContentCanal/LocationTitleView.h
@@ -0,0 +1,17 @@
+//
+//  LocationTitleView.h
+//  
+//
+//  Created by Sam Lau on 6/14/15.
+//
+//
+
+#import "SHPAbstractView.h"
+
+@interface LocationTitleView : SHPAbstractView
+
+@property (strong, nonatomic) UILabel *titleLabel;
+@property (strong, nonatomic) UIImageView *downwardDirectionImageView;
+@property (strong, nonatomic) UILabel *locationLabel;
+
+@end
